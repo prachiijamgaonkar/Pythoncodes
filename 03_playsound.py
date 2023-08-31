908ca2399c78213709ff0604f617029e79c05d30
prachiijamgaonkar <117511975+prachiijamgaonkar@users.noreply.github.com>
2023-08-31 20:06:53 +0530

    Update 03_playsound.py

--- 03_playsound.py
+++ 03_playsound.py
@@ -1,7 +1,6 @@
 #Q2 Install an external module and use it to perform the opration of your interest
 
 from playsound import playsound
-playsound("C:\\Users\\pravin jamgaonkar\\Desktop\\PythonPrachi\\play.mp3")
+playsound("C:\\Users\\prachijamgaonkar\\Desktop\\PythonPrachi\\play.mp3")
 
 
-##i can't get over it....sry
