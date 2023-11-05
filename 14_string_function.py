0635fcecbb91388d5edc18df3d8129aaa0cd556b
prachiijamgaonkar <117511975+prachiijamgaonkar@users.noreply.github.com>
2023-11-05 17:06:00 +0530

    Update 14_string_function.py

--- 14_string_function.py
+++ 14_string_function.py
@@ -1,10 +1,10 @@
 story = "hlw everyone, I am prachi jamgaonkar currently  pursuing B.Tech from SB Jain Institute of technology and management ,Nagpur ."
 
-print(len(story))
-print(story.endswith("."))
-print(story.count("pr"))
-print(story.capitalize())#it capitalize only first character of whole string or sentence
-print(story.find("hlw"))
+print(len(story))#it count the number of character of sentence
+print(story.endswith("."))#it returns true or false
+print(story.count("pr"))#it count how many times it in sentence
+print(story.capitalize())#it capitalize only first character of first word of sentence
+print(story.find("hlw"))#it gives indexing of hlw in the sentence
 print(story.replace("prachi","PRACHI"))
 
 
